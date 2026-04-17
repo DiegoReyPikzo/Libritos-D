075cf5c93c347c1f2f1ac80be2984bfb111697b2
DiegoReyPikzo <20253rd033@utez.edu.mx>
2026-04-17 14:40:12 -0600

    STengo la clase libro completa

--- src/main/java/com/example/biblio_teca/model/Libro.java
+++ src/main/java/com/example/biblio_teca/model/Libro.java
@@ -14,4 +14,34 @@ public Libro(int id, String title, String autor){
     this.title = title;                                                 //Declaramos las variables mejor
     this.autor = autor;
 }
+    public int getNumeroLibro() {
+        return id;
+    }
+
+    public void setNumeroLibro(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return title;
+    }
+
+    public void setTitulo(String title) {
+        this.title = title;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Número: " + id + " | Título: " + title + " | Autor: " + autor;
+    }
+}
+
 }
